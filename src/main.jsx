027b233f356f6e94eb681e65f9e7501eb02ac01d
tkdalsss <lsm991229@naver.com>
2024-06-20 15:35:49 +0900

    PWA 및 Service Worker 적용

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Modal from "react-modal";
+import registerServiceWorker from "./register-sw.jsx";
 
 Modal.setAppElement("#root");
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-);
+registerServiceWorker(`/service-worker.js`);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
