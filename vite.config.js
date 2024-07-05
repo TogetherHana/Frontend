f8052a13541c6ec68d317556e69579b8393ac2b0
tkdalsss <lsm991229@naver.com>
2024-07-05 16:30:05 +0900

    fix: call sw

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: "./index.html",
-        sw: "/public/service-worker.js"
+        sw: "/service-worker.js"
       }
     }
   }
