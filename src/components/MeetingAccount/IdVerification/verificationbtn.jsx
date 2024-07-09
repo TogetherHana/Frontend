2a15df00415892bdbd0551086851b4f982a3e6ba
egon6018 <goeun6018@naver.com>
2024-07-09 09:45:56 +0900

    - 필요한내용 수정완료

--- src/components/MeetingAccount/IdVerification/verificationbtn.jsx
+++ src/components/MeetingAccount/IdVerification/verificationbtn.jsx
@@ -3,7 +3,7 @@ import Button from "@/components/Button";
 import { useNavigate } from "react-router-dom";
 
 function VerificationBtn({ params }) {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { btnText, btnBelowText, onClick, cnm } = params;
 
   return (
@@ -12,7 +12,19 @@ function VerificationBtn({ params }) {
         {btnText}
       </Button>
       {btnBelowText && (
-        <div className="idVerification below">{btnBelowText}</div>
+        <div
+          className="idVerification below"
+          onClick={() =>
+            navigate("/maccount/register/processing", {
+              state: {
+                url: "/maccount/register/idcertification",
+                text: "확인중입니다"
+              }
+            })
+          }
+        >
+          {btnBelowText}
+        </div>
       )}
     </div>
   );
